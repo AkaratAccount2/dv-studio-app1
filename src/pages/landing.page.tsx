9ea652ad6d4a3a451c70b601c36145a26395417d
akarat <akrs33@gmail.com>
2023-01-06 23:58:26 +0700

    Dev: add logo

--- src/pages/landing.page.tsx
+++ src/pages/landing.page.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import LogInPage from './login/login.page'
+import { Image } from 'antd'
+
+const imageUrl = `${process.env.PUBLIC_URL}/apple-icon-180x180.png`;
 
 export function LandingPage() {
-    return  (
+    return (
         <div style={{ paddingTop: '38px', textAlign: 'center' }}>
             <div className="welcome" style={{ paddingBottom: '20px' }}>
-                <div style={{ fontSize: '24px', fontWeight: 'bold' }}>Davinci Studio</div>
-                <div style={{ fontSize: '12px', fontWeight: 'bold', marginLeft: '-2px' }}>(Art Education School)</div>
+                <div style={{ marginBottom: '0px' }}>
+                    <Image preview={false}
+                        width={180}
+                        src={imageUrl}
+                    />
+                </div>
+                {/* <div style={{ fontSize: '24px', fontWeight: 'bold' }}>Davinci Blue Studio</div> */}
+                <div style={{ fontSize: '12px', fontWeight: 'bold', marginLeft: '-2px' ,marginTop: '0px' }}>(Art Gallary & Education School)</div>
             </div>
             <LogInPage />
         </div>
